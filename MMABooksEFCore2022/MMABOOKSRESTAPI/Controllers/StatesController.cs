59616b0c30f2986fe13935dd63e007abf54d140b
tuftsJ <166471769+tuftsJ@users.noreply.github.com>
2024-11-24 19:28:45 -0800

    Added StatesController

---
+++ MMABooksEFCore2022/MMABOOKSRESTAPI/Controllers/StatesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MMABooksEFClasses.Models;
+
+namespace MMABOOKSRESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatesController : ControllerBase
+    {
+        private readonly MMABooksContext _context;
+
+        public StatesController(MMABooksContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/States
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<State>>> GetStates()
+        {
+          if (_context.States == null)
+          {
+              return NotFound();
+          }
+            return await _context.States.ToListAsync();
+        }
+
+        // GET: api/States/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<State>> GetState(string id)
+        {
+          if (_context.States == null)
+          {
+              return NotFound();
+          }
+            var state = await _context.States.FindAsync(id);
+
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return state;
+        }
+
+        // PUT: api/States/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutState(string id, State state)
+        {
+            if (id != state.StateCode)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(state).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/States
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<State>> PostState(State state)
+        {
+          if (_context.States == null)
+          {
+              return Problem("Entity set 'MMABooksContext.States'  is null.");
+          }
+            _context.States.Add(state);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (StateExists(state.StateCode))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetState", new { id = state.StateCode }, state);
+        }
+
+        // DELETE: api/States/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteState(string id)
+        {
+            if (_context.States == null)
+            {
+                return NotFound();
+            }
+            var state = await _context.States.FindAsync(id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            _context.States.Remove(state);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StateExists(string id)
+        {
+            return (_context.States?.Any(e => e.StateCode == id)).GetValueOrDefault();
+        }
+    }
+}
